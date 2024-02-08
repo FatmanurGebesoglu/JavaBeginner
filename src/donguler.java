e9677626e5fa29441aa3117e4652856ec38a004b
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-08 19:48:58 +0300

    metotlar.java oluşturuldu

--- src/donguler.java
+++ src/donguler.java
@@ -3,5 +3,17 @@ class donguler {
         for(int i=1; i<10; i++){
             System.out.println("bu bir döngü"+ i);
         }
+
+        int sayi = 10;
+        while(sayi<=100){
+            System.out.println(sayi);
+            sayi= sayi+10;
+        }
+
+        int sayi2=10;
+        do{
+            System.out.println("do çalıştı");
+        }while (sayi2>100);
+
     }
 }
