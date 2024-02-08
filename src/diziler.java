3f6cd6d85f5b2372d76bad2e4b8c2381f293e286
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-08 20:25:02 +0300

    diziler.java olusturuldu

---
+++ src/diziler.java
@@ -0,0 +1,11 @@
+class diziler {
+    public static void main(String[] args){
+        String[] sehirler1 = new String[] {"Ankara", "İstanbul", "İzmir"};
+
+        //foreach
+
+        for(String sehir : sehirler1){
+            System.out.println(sehir);
+        }
+    }
+}
--- src/metotlar.java
+++ src/metotlar.java
@@ -1,5 +1,25 @@
 class metotlar {
     public static void main(String[] args){
-
+        mesajVer("Fatmanur");
+        hesapla();
+        topla(5,4);
     }
+    public static void mesajVer(String isim){
+        System.out.println("merhaba"+isim);
+    }
+    public static void hesapla(){
+        System.out.println("hesaplandı");
+    }
+    public static int topla(int sayi1, int sayi2){
+        int toplam=sayi1+sayi2;
+        return toplam;
+    }
+    //toplanan iki sayinin %10 unu bul
+    public static void toplananIkiSayininYuzdeOnu(int sayi1, int sayi2){
+        int toplam= topla(sayi1,sayi2);
+        double sonuc= (toplam * 10)/100;
+        System.out.println("Sonuç:" + sonuc );
+    }
+
+
 }
