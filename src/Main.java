f94d7d422f1dce9127aaa02f100d363635fc5ff9
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-02-08 18:38:06 +0300

    sart && donguler

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int not = 55; //integer
+        //System.out.println(not);
+        double notOrtalamasi = 50.5;
+        String isim = "Fatmanur";
+        boolean isAuthentication= true;
+        //isimlendireme camelCase
+        //class PascalCase ile isimlendirilir
+
+    }
+}
